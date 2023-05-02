27133e0a8f9f2d68f8d52bf3783bd25a82088047
ahmad abou-saleh <ahmed.mu.mas@gmail.com>
2023-05-01 22:54:34 -0700

    changed json field

--- urlshorten/src/api/index.js
+++ urlshorten/src/api/index.js
@@ -3,7 +3,7 @@ export async function postURL(longUrl) {
     const response = await fetch("http://localhost:10000/api/v1/short", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({url: longUrl})
+        body: JSON.stringify({long_url: longUrl})
     })
     return await response.json()
 }
